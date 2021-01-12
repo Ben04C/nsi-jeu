6642afb02bf59f274ba8eb26e0028bc0116f4ce1
bench <bj04.ch@gmail.com>
2021-01-12 14:40:36 +0100

    12-01

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pygame.locals import *
 pygame.init()
 pygame.mixer.init()
 i=0
+progress=0
 
 # create the window
 
@@ -21,16 +22,17 @@ pygame.display.set_icon(icon)
 
 progress = 0 #Progress is going to be the save state of the game. When a scene is finished there will be progress+=1 and the scene will change with an if progress ==.
 #Assets:
-dialogueFont = pygame.font.Font("assets/fonts/8-BIT WONDER.TTF", 10)
+dialogueFont = pygame.font.Font("assets/fonts/8-BIT WONDER.TTF", 15)
 
 
 
 
 
 #Characters
-playerImg = pygame.image.load("assets/detective-hat.png").convert_alpha()
+playerImg = pygame.image.load("assets\player\dualogue/Game_Character_Half_Body_OUTSIDE_LIGHTING_big_res.png").convert_alpha()
 playerX = 370
 playerY = 480
+playerDialogue = pygame.image.load("assets\player\dualogue/Game_Character_Half_Body_OUTSIDE_LIGHTING_big_res.png").convert_alpha()
 
 
 
@@ -73,8 +75,10 @@ keys = pygame.key.get_pressed()
 def player(x, y):
     screen.blit(playerImg, (x, y))
     if keys[pygame.K_RIGHT]:
+        global playerX
         playerX += 5
     if keys[pygame.K_LEFT]:
+        global playerY
         playerY += 5
 
 
@@ -110,13 +114,25 @@ def cutscene1():
     pygame.mixer.music.play()
     pygame.time.delay(1 * 5000)
 
+def oftenusedD1():
+    screen.blit(crimeSceneBG, (0, 0))
+    screen.blit(playerDialogue, (0, 0))
+    screen.blit(textbox, (200, 450))
 
+
+
+def checkanykey():
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            global progress
+            progress += 1
+            print("click")
 
 #Game loop
 running = True
 while running:
     for event in pygame.event.get(): #checks if cross is clicked.
-        if event.type == pygame.QUIT:
+        if event.type == QUIT:
 
             running = False
             print("croix")
@@ -125,22 +141,42 @@ while running:
             
     
     if progress == 0:
-        screen.blit(crimeSceneBG,(0,0))
-        screen.blit(textbox,(640,500) )
-        screen.blit(explanation1, (640,500))
+        checkAnyKey=True
+        oftenusedD1()
+        screen.blit(explanation1, (250,500))
         pygame.display.update()
         pressed = pygame.key.get_pressed()
-        
-    
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN :
-                progress+=1
-                print("click")
+        checkanykey()
+
             
             
     if progress == 1:
-        screen.blit(crimeSceneBG, (0, 0))
-        toprint = dialogueFont.render("yay works", True, (255, 255, 255))
-        screen.blit(toprint, (0, 0))
+        oftenusedD1()
+        toprint = dialogueFont.render("Huf... Puff..., Huf..., Puff. Palms sweaty, this case seems", True, (255, 255, 255))
+        screen.blit(toprint, (250,500))
+        toprint = dialogueFont.render("to be big. AND Why is it always me that has to be called ", True, (255, 255, 255))
+        screen.blit(toprint, (250, 520))
+        toprint = dialogueFont.render("upon at 2am.", True, (255, 255, 255))
+        screen.blit(toprint, (250, 540))
         pygame.display.update()
-    
+        checkanykey()
+
+    if progress == 2:
+        oftenusedD1()
+        toprint = dialogueFont.render("My name is Richard Bright, homocide detective at the  ", True, (255, 255, 255))
+        screen.blit(toprint, (250,500))
+        toprint = dialogueFont.render("localprecinct from Taga, Mr. Taga himself called me this", True, (255, 255, 255))
+        screen.blit(toprint, (250, 520))
+        toprint = dialogueFont.render("Let me tell you I got a ", True, (255, 255, 255))
+        pygame.display.update()
+        checkanykey()
+
+    if progress == 3:
+        oftenusedD1()
+        toprint = dialogueFont.render("Policeman :I am sorry to have warned you  this late", True, (255, 255, 255))
+        screen.blit(toprint, (250, 500))
+        toprint = dialogueFont.render("but the matter is urgent !", True, (255, 255, 255))
+        screen.blit(toprint, (250, 520))
+        pygame.display.update()
+        checkanykey()
+
