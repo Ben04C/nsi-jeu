41bd2d248927edfb3c7e27bdccc5c76c195aa404
Scripty ツ <70953225+ScriptyShake@users.noreply.github.com>
2021-02-03 12:01:32 +0100

    Add files via upload
    Temporary changes. Will merge with main.py later.

---
+++ main_temp.py
@@ -0,0 +1,286 @@
+import time
+import pygame
+
+from pygame.locals import *
+
+from game import Game
+
+j=0
+g = Game()
+animateWalking = False
+
+while g.running:
+    g.curr_menu.display_menu()
+    g.game_loop() #opens menu
+
+# Initialisation
+pygame.init()
+pygame.mixer.init()
+i=0
+walkingRight=True
+#Clock for FPS
+clock = pygame.time.Clock()
+
+
+# create the window
+
+screen = pygame.display.set_mode((1280,720))
+
+#creating the clock
+message_end_time = 0
+
+
+#Title and Icon
+pygame.display.set_caption("Cyberpunk style game")
+icon = pygame.image.load("assets/detective-hat.png")
+pygame.display.set_icon(icon)
+
+progress = 0 #Progress is going to be the save state of the game. When a scene is finished there will be progress+=1 and the scene will change with an if progress ==.
+#Assets:
+dialogueFont = pygame.font.Font("assets/fonts/EightBitDragon-anqx.ttf", 15)
+
+
+
+
+
+#Characters
+playerX = 370
+playerY = 480
+playerDialogue = pygame.image.load("assets\player\half_bodies/Game_Character_Half_Body_OUTSIDE_LIGHTING_big_res.png").convert_alpha()
+
+
+#Sprites
+#Idle
+playerImgR = pygame.image.load("assets\player\sprites\detective_idle\spr_detective_idle_0.png").convert_alpha()
+playerImgL = pygame.transform.flip(playerImgR, True, False).convert_alpha()
+#Walk
+#liste contenant les iamges du joueur afin de faire l'animation
+playerWalkR = [pygame.image.load("assets\player\sprites\detective_walk\spr_detective_walk_0.png").convert_alpha(), pygame.image.load("assets\player\sprites\detective_walk\spr_detective_walk_1.png").convert_alpha(), pygame.image.load("assets\player\sprites\detective_walk\spr_detective_walk_2.png").convert_alpha(), pygame.image.load("assets\player\sprites\detective_walk\spr_detective_walk_3.png").convert_alpha()]
+
+playerWalkL = [pygame.transform.flip(playerWalkR[0], True, False).convert_alpha(), pygame.transform.flip(playerWalkR[1], True, False).convert_alpha(), pygame.transform.flip(playerWalkR[2], True, False).convert_alpha(), pygame.transform.flip(playerWalkR[3], True, False).convert_alpha()]
+
+#PoliceImg = pygame.image.load("police.png")
+PoliceX = 400
+PoliceY = 480
+
+
+
+
+
+
+#Backgrounds
+crimeSceneBG = pygame.image.load("assets/backgrounds/Game_First_Scene_bigger_res.png").convert()
+
+
+#UI:
+textbox = pygame.image.load("assets/UI/textbox_full_res.png").convert_alpha()
+inventaire = pygame.image.load("assets/UI/inventaire.png").convert_alpha()
+
+
+
+
+
+#sound
+#def m_buzzing():
+    #pygame.mixer.music.load("assets/sound/SFX/Neon_light_Buzzing.mp3")
+#def m_investigation():
+    #pygame.mixer.music.load("assets/sound/Music/SFX/Neon_light_Buzzing.mp3")
+#def m_explosion():
+    #pygame.mixer.music.load("assets/sound/SFX/small-explosion.mp3")
+
+
+
+
+
+
+keys = pygame.key.get_pressed()
+
+
+
+
+
+
+
+#dialogues:
+Dialogue1p=["Policeman ;I am sorry to have warned you  this late but the matter is urgent !", "some other text"]
+explanation1= dialogueFont.render("What is going on here… (Press any key to continue.)", True, (255, 255, 255))
+
+
+
+
+
+def cutscene1():# This cutscene is finally not used for now. We keep it just in case we have some more time.
+    cscene1d=["Flicker", "Flicker", "Flicker", "You're a goner"]
+    pygame.mixer.music.set_volume(0.7)
+    for i in range(4): #Makes the screen blink
+
+        pygame.time.delay(1 * 3000)
+        screen.fill((128, 0, 128))
+        pygame.mixer.music.play()
+        pygame.display.update()
+        screen.fill((0, 0, 0))
+        pygame.time.delay(1 * 3000)
+        pygame.display.update()
+        dialogue = dialogueFont.render(cscene1d[i], True, (255, 255, 255))
+        screen.blit(dialogue, (0, 0))
+    m_explosion()
+    pygame.mixer.music.play()
+    pygame.time.delay(1 * 5000)
+
+def oftenusedD1(): #This is a function that executes the lines of code that are often used in the first dialogue.
+    screen.blit(crimeSceneBG, (0, 0))
+    screen.blit(playerDialogue, (0, 0))
+    screen.blit(textbox, (200, 450))
+
+
+
+def checkanykey(): #This function checks if any key is pressed on the keyboard.
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            global progress
+            progress += 1
+            print("click")
+
+def boundaries(): #teleports the player back into the game boundaries if he tries to get out
+    global playerX
+    if playerX <= 0:
+        playerX = 1
+    if playerX >= 1150:
+        playerX = 1149
+
+def walking_function():#vérifie si le joueur est en train de marcher et dans quelle dirrection
+    global animateWalking
+    global j
+    global walkingRight
+    animateWalking = False  # permet de dire que l'on de veut pas animer le joueur.
+
+    keys = pygame.key.get_pressed()  # on fait un dictionnaire avec les valeurs de pygame.keys.get_pressed()
+
+    if keys[pygame.K_RIGHT]:  # Si la valeur de la clé K_RIGHT est vraie:
+
+        j += 1
+        if j <= 4:
+            j = 0
+            animateWalking = True
+
+    if keys[pygame.K_LEFT]:
+
+        j += 1  # la variable temporaire j permet de voir si touche est maintenue pendant assez longtemps pour lancer une animation
+        if j <= 4:
+            j = 0
+            animateWalking = True
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_RIGHT:
+                walkingRight = True
+            if event.key == K_LEFT:
+                walkingRight = False
+    screen.blit(crimeSceneBG, (0, 0))
+
+
+
+
+
+
+
+
+#Game loop
+running = True
+while running:
+    for event in pygame.event.get(): #checks if cross is clicked.
+        if event.type == QUIT:
+
+            running = False
+            print("croix")
+    clock.tick(30) #FPS
+
+
+            
+    
+    if progress == 0:
+        checkAnyKey=True
+        oftenusedD1()
+        screen.blit(explanation1, (250,500))#écrit le premier texte visible à l'écran
+        pygame.display.update()
+        checkanykey()#appele la fonction checkanykey() qui permet de vérifier si une touche du clavier est cliquée.
+
+            
+            
+    if progress == 1:
+        oftenusedD1()
+        toprint = dialogueFont.render(" Why is it always me getting called at 2 AM?", True, (255, 255, 255))#Ceci sont les paramètres du texte à afficher
+        screen.blit(toprint, (250,500))#Ceci affiche le texte
+        pygame.display.update()
+        checkanykey()
+
+    if progress == 2:
+        oftenusedD1()
+        toprint = dialogueFont.render("Ah well… I guess it is my job after all… ", True, (255, 255, 255))
+        screen.blit(toprint, (250,500))
+        toprint = dialogueFont.render("Well then, let’s see what’s going on.", True, (255, 255, 255))
+        pygame.display.update()
+        checkanykey()
+
+    if progress == 3:
+        oftenusedD1()
+        toprint = dialogueFont.render("I see a Police officer standing at the end of the road.", True, (255, 255, 255))
+        screen.blit(toprint, (250, 500))
+        pygame.display.update()
+        checkanykey()
+
+    if progress == 4:
+        oftenusedD1()
+        toprint = dialogueFont.render("Let’s see if he has something to tell us.", True, (255, 255, 255))
+        screen.blit(toprint, (250, 500))
+        pygame.display.update()
+        checkanykey()
+
+
+
+        
+    if progress== 5:#phase d'enquete
+        walking_function()
+        if animateWalking == True:
+            if walkingRight==True:
+                for counter in range(len(playerWalkR)):
+                    walkR = playerWalkR[int(counter)]
+                    counter = counter + 1
+                    screen.blit(crimeSceneBG, (0, 0))
+                    playerX = playerX + 40
+                    boundaries()
+                    screen.blit(walkR, (playerX, 350))
+                    pygame.display.update()
+                    time.sleep(0.2)
+
+            else:
+                for counter in range(len(playerWalkL)):
+                    walkL = playerWalkL[int(counter)]
+                    counter = counter + 1
+                    screen.blit(crimeSceneBG, (0, 0))
+                    playerX = playerX - 40
+                    boundaries()
+                    screen.blit(walkL, (playerX, 350))
+                    pygame.display.update()
+                    time.sleep(0.2)
+
+
+        else:
+            if walkingRight==False:
+                screen.blit(playerImgL,(playerX, 350))
+                pygame.display.update()
+            else:
+                screen.blit(playerImgR, (playerX, 350))
+                pygame.display.update()
+    
+    for event in pygame.event.get():
+        inventory=False
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_TAB and inventory==False:
+                inventory = True
+                screen.blit(inventaire, (0, 0))
+                pygame.display.update()
+            if event.key == K_TAB and inventory==True:
+                inventory = False
+                screen.blit(inventaire, (0, 0))
+                pygame.display.update()
