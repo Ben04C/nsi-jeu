a6b9124085821aee9ccd66a78634624b5a11c118
bench <bj04.ch@gmail.com>
2021-05-07 19:24:10 +0200

    fixed issue with encoding

--- csv_dialogue.py
+++ csv_dialogue.py
@@ -13,7 +13,7 @@ def dialogue():
     Policeman_Before_Bush_Dialogue = []
 
 
-    with open('assets/Dialogues/MainDialogues.csv') as csvfile: #We open the csv file with the dialogues
+    with open('assets/Dialogues/MainDialogues.csv', "r" , encoding='UTF-8') as csvfile: #We open the csv file with the dialogues
         reader = csv.reader(csvfile, delimiter=',')
         for item in reader:
             MainDialogues.append(item) #We add every item from the list into a list
@@ -22,15 +22,18 @@ def dialogue():
     MainDialogues = MainDialogues[0]
     #end of MainDialogue
 
-    with open('assets/Dialogues/Flowerpot.csv') as csvfile: #We open the csv file with the dialogues
+    with open('assets/Dialogues/Flowerpot.csv', "r" , encoding='UTF-8') as csvfile: #We open the csv file with the dialogues
         reader = csv.reader(csvfile, delimiter=',')
+        print(reader)
         for item in reader:
+            print(item)
             FlowerPotDialogue.append(item) #We add every item from the list into a list
+
     csvfile.close()
     FlowerPotDialogue = FlowerPotDialogue[0]
     #end of FlowerPotDialogue
 
-    with open('assets/Dialogues/Stopsign.csv') as csvfile: #We open the csv file with the dialogues
+    with open('assets/Dialogues/Stopsign.csv', "r" , encoding='UTF-8') as csvfile: #We open the csv file with the dialogues
         reader = csv.reader(csvfile, delimiter=',')
         for item in reader:
             StopSignDialogue.append(item) #We add every item from the list into a list
@@ -38,7 +41,7 @@ def dialogue():
     StopSignDialogue = StopSignDialogue[0]
     #end of StopSignDialogue
 
-    with open('assets/Dialogues/Policeman_Before_Bush_Inspect.csv') as csvfile: #We open the csv file with the dialogues
+    with open('assets/Dialogues/Policeman_Before_Bush_Inspect.csv', "r" , encoding='UTF-8') as csvfile: #We open the csv file with the dialogues
         reader = csv.reader(csvfile, delimiter=',')
         for item in reader:
             Policeman_Before_Bush_Dialogue.append(item) #We add every item from the list into a list
@@ -46,7 +49,7 @@ def dialogue():
     Policeman_Before_Bush_Dialogue = Policeman_Before_Bush_Dialogue[0]
     #end of Policeman_Before_Bush_Dialogue
 
-    with open('assets/Dialogues/Policeman_After_Bush_Inspect.csv') as csvfile: #We open the csv file with the dialogues
+    with open('assets/Dialogues/Policeman_After_Bush_Inspect.csv', "r" , encoding='UTF-8') as csvfile: #We open the csv file with the dialogues
         reader = csv.reader(csvfile, delimiter=',')
         for item in reader:
             Policeman_After_Bush_Dialogue.append(item) #We add every item from the list into a list
